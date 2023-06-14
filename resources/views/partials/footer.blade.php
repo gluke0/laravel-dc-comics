07dbd4e140fbe76350b0fdfe1c5bd832bc2b4321
gluke0 <milanesigianluca@gmail.com>
2023-06-14 15:46:18 +0200

    import imgs, header, footer from old project

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,81 @@
+<footer>
+    <div class="container position-relative">
+        <div class="footer-top">
+            <div class="list d-flex">
+                <div class="col">
+                    <div class="footmenu">
+                        <h2 class="text-uppercase">dc comics</h2>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a class="text-uppercase" href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+                    <div class="footmenu">
+                        <h2 class="text-uppercase">shop</h2>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="footmenu">
+                        <h2 class="text-uppercase">dc</h2>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy Policy(New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="footmenu">
+                        <h2 class="text-uppercase">sites</h2>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                {{-- <div class="boxxi">
+                    All Site Content TM and &COPY; 2020 DC Entertainment, unless otherwise <span class="text-primary"> noted here </span>. All rights reserved.
+                    <span class="text-primary"> Cookies Settings</span>
+                </div> --}}
+            </div>
+            <div class="bg-logo">
+                <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="dc logo">
+            </div>
+        </div>
+    </div>
+    <div class="social">
+        <div class="footer-down container d-flex justify-content-between align-items-center">
+            <button class="text-uppercase">sign up now!</button>
+            <div class="social-buttons d-flex justify-content-between align-items-center">
+                <h4 class="text-uppercase">follow us</h4>
+                <ul>
+                    <li><a href="#"><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook logo"></a></li>
+                    <li><a href="#"><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter logo"></a></li>
+                    <li><a href="#"><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="youtube logo"></a></li>
+                    <li><a href="#"><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest logo"></a></li>
+                    <li><a href="#"><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope logo"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,67 @@
+<header>
+    <div class="bg-primary text-white text-end">
+        <div class="container">
+            <span class="text-uppercase me-5">dc power visa&REG;</span>
+            <span class="dropdown">
+                <span class="dropdown-toggle text-uppercase" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    additional dc sites
+                </span>
+                <ul class="dropdown-menu">
+                </ul>
+            </span>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a href="/">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC Comics Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav me-3">
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">characters</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">collectibles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">fans</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">news</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-uppercase" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            shop
+                        </a>
+                        <ul class="dropdown-menu">
+                        </ul>
+                    </li>
+                </ul>
+                <form action="" method="">
+                    <input type="text" placeholder="Search" class="text-end">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </form>
+            </div>
+        </div>
+    </nav> 
+    <div class="container-fluid" id="jumbotron"></div> 
+</header>
