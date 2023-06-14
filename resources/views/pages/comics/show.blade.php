a69ee310c817ec76f0cc409f2d3a04427fa4e0e2
gluke0 <milanesigianluca@gmail.com>
2023-06-14 17:59:34 +0200

    comics.show created

---
+++ resources/views/pages/comics/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section ('title-page')
+    DC Comics | Comic
+@endsection
+
+@section('content')
+
+<div class="container-fluid">
+    single comic 
+</div>
+
+@endsection
