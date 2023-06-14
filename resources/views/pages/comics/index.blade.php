0c3043602f20041372103b4f2b078a6c07360a40
gluke0 <milanesigianluca@gmail.com>
2023-06-14 17:13:40 +0200

    changed button name

--- resources/views/pages/comics/index.blade.php
+++ resources/views/pages/comics/index.blade.php
@@ -17,33 +17,7 @@
             @endforeach --}}
 
         </div>
-        <span id="button" class="text-uppercase">load more</span>
-    </div>
-    <div class="container-fluid" id="buy">
-        <div class="container p-4" id="buy-section">
-            <div class="row text-white justify-content-center">
-                <div class="col-2 d-flex align-items-center text-uppercase">
-                    <img class="me-2" style="width:20%" src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="digital comics">
-                    digital comics
-                </div>
-                <div class="col-2 d-flex align-items-center text-uppercase">
-                    <img class="me-2" src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="merchandise">
-                    dc merchandise
-                </div>
-                <div class="col-2 d-flex align-items-center text-uppercase">
-                    <img class="me-2" class="shrink" src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="subscriptions">
-                    subscription
-                </div>
-                <div class="col-2 d-flex align-items-center text-uppercase">
-                    <img class="me-2" style="width:20%" src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="shop locator">
-                    comic shop locator
-                </div>
-                <div class="col-2 d-flex align-items-center text-uppercase">
-                    <img class="me-2" src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="dc power">
-                    dc power visa
-                </div>
-            </div>
-        </div>
+        <span id="button" class="text-uppercase">create your comic</span>
     </div>
 </div>
 
